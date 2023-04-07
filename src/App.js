fd95d0373d8730b1d1feda8ced11141a6e900a7b
Ravi Lour <ravijaat2917@gmail.com>
2023-04-07 19:59:21 +0530

    Day 3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 import React from "react";
-import { Component } from "react";
+import Student from "./Student";
+// import { Component } from "react";
+
+// let element = React.createElement("h1", null, "Hello Students");
+
+// 1. Class Based Component
+
+// class App extends Component {
+//   render() {
+//     return (
+//       // Also use Fragment as imported From react as import {Fragment} from 'react'
+//       <React.Fragment>
+//         <h1>Hello {this.props.name}</h1>
+//         <h1>Hello Students</h1>
+//       </React.Fragment>
+//     );
+//   }
+// }
 
 
-// let el = React.createElement("h1", null, "Hello Students");
+// 2. Function Based Component
 
-class App extends Component {
-    render(){
-        return (
-            <>
-            <h1>Hello Students</h1>
-            </> 
-        )
-    }
+// const App = (props) =>{
+//     return <h1>Hello {props.name} .</h1>
+// }
+
+
+
+// ------------------------------------------------------------
+// Composing Components...
+const App = () =>{
+    return (
+      <>
+        <Student name="Ravi" />
+        <Student name="Sumit" />
+        <Student name="Sonam" />
+      </>
+    );
 }
 
-export default App; 
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App.js';
 
-ReactDom.render(<App/> , document.getElementById("root"));
+
+// 1.For Class Based Component
+// ReactDom.render(<App/> , document.getElementById("root"));
+
+// 2. For function Based Component includes Props
+ReactDom.render(<App name="Ravi" />, document.getElementById("root"));
