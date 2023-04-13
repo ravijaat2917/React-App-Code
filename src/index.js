a3b95bf44451c71989c86337cf699e0fd06d4cb7
Ravi Lour <ravijaat2917@gmail.com>
2023-04-13 13:26:11 +0530

    index.js code modified

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
+import ConditionalRendering from './05_ConditionalRendring/App.js';
 import UnMounted from "./03_UnMounting_Component/Student";
 
 // 1.For Class Based Component
 // ReactDom.render(<App/> , document.getElementById("root"));
 
+
 // 2. For function Based Component includes Props
-ReactDOM.render(<App name="Ravi" />, document.getElementById("root"));
+// ReactDOM.render(<App name="Ravi" />, document.getElementById("root"));
+
 
 // 3. for mount and Unmount Code rendering...
 // ReactDOM.render(<UnMounted />, document.getElementById("unMount"));
 // ReactDOM.unmountComponentAtNode(document.getElementById("unMount"));
+
+
+
+// 4. Conditional Rendering Code...
+ReactDOM.render(
+  <ConditionalRendering consumer={true} primeMember={true} />,
+  document.getElementById("root")
+);
